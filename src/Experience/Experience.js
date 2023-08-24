26909ae52ee29adeb11515fb2e9e7b5d9db367a9
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-08-24 21:28:48 +0200

    [add] world class, env class and resource class

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -4,6 +4,8 @@ import Sizes from "./Utils/Sizes"
 import Time from "./Utils/Time"
 import Camera from './Camera'
 import Renderer from './Renderer'
+import World from './World/World.js'
+import Resources from './Utils/Resources'
 
 let instance = null
 
@@ -28,8 +30,10 @@ export default class Experience
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
+    this.resources = new Resources
     this.camera = new Camera()
     this.renderer = new Renderer()
+    this.world = new World()
 
     //Sizes change event
     this.sizes.on('resize', () =>
